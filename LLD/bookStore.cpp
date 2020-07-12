d93aed7ff5879e606f28010bb56a3348f9959949
akhil <akhiltiwari.13@gmail.com>
2020-07-12 11:59:29 +0530

    initial structure for the bookStore

--- LLD/bookStore.cpp
+++ LLD/bookStore.cpp
@@ -1,3 +1,59 @@
 #include <bits/stdc++.h>
+#include <chrono>
+#include <cstdint>
+#include <vector>
 using namespace std;
+
+class BookData {
+public:
+  BookData(uint16_t id, std::string author_name, uint16_t shelf,
+           uint16_t cabinet)
+      : id(id), authorName(author_name), cabinet(cabinet), shelf(shelf) {}
+
+  uint16_t getId() { return this->id; }
+  uint16_t getShelf() { return this->shelf; }
+  uint16_t getCabinet() { return this->cabinet; }
+  std::string getAuthorName() { return this->authorName; }
+  std::string getBookName() { return this->bookName; }
+
+private:
+  uint16_t id;
+  std::string bookName;
+  std::string authorName;
+  uint16_t shelf;
+  uint16_t cabinet;
+};
+
+// a DTO perhaps?
+struct BookSearchParam {
+  uint16_t id;
+  std::string bookName;
+};
+
+// global function to fetch data from records.
+vector<BookData> getBookData(const BookSearchParam &param) {
+  vector<BookData> searchResult;
+  // search logic?
+
+  return searchResult;
+}
+
+/* components of the API to be exposed. */
+struct FindBookRequest {
+  std::string bookName;
+  std::string authorName;
+};
+
+struct FindBookResponse {
+  uint16_t shelf;
+  uint16_t cabinet;
+};
+
+vector<FindBookResponse> findBook(const FindBookRequest &param) {
+  vector<FindBookResponse> apiSearchResult;
+  // search logic?
+
+  return apiSearchResult;
+}
+
 int main() { return 0; }
